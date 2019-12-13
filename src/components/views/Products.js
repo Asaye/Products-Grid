230a62f7abac6ea08e66dd5e4a6beacd2039785e
Asaye <asayechemeda@yahoo.com>
2019-12-13 23:15:07 +0300

    the products grid styling done, the scrolling part started

--- src/components/views/Products.js
+++ src/components/views/Products.js
@@ -4,20 +4,24 @@ import Axios from 'axios';
 import '../css/products.css';
 
 const MILLIS_PER_DAY = 24 * 3600 * 1000;
+const DAYS = ["Today", "One", "Two", "Three", "Four", "Five", "Six"];
 
 class Products extends Component {
   state = {
     products: null,
+    nextBatch: null,
+    lastPage: 1,
     pages: []
   } 
-  
+  constructor(props) {
+    super(props);
+    this._prevOffsetBottom = 1;
+  }
   _getDate = (str) => {
      const date = new Date(str);
      const now = new Date();
      const days = parseInt((now.getTime() - date.getTime())/MILLIS_PER_DAY);
-     if (days === 0) return "Today";
-     else if (days === 1) return `${days} day ago`;
-     else if (days < 7) return `${days} days ago`;
+     if (days < 7) return `${DAYS[days]} ${days > 1 ? " days ago" : days === 1 ? "day ago" : ""}`;
      else return `${date.getMonth() + 1}-${date.getDate() < 10 ? ("0"+date.getDate()) : date.getDate() }-${date.getFullYear()}`
   }
   _getNextAd = () => {
@@ -28,8 +32,55 @@ class Products extends Component {
       this._prevAd = nextAd;
       return nextAd;
   }
+  _handleScroll = (start) => {
+    if (document.querySelector(".products-container") === null) return;
+    const el = document.querySelector(".products-container").lastChild;
+    if (el === null) return;
+    const rect = el.previousSibling.getBoundingClientRect();
+    const ht = document.documentElement.clientHeight;
+    const offsetTop = ht - rect.y;
+    const offsetBottom = ht - rect.y - rect.height;
+    console.log(offsetBottom + "  " + this._prevOffsetBottom + "   " + this.state.lastPage);
+    //if (this.state.page === this.state.lastPage*4) offsetBottom -= 360;
+    if (offsetBottom >= 0 && this._prevOffsetBottom < 0 || start && offsetBottom > 0) {
+      //add one row
+      Axios.get(`http://localhost:3001/products/?_page=${this.state.lastPage}&_limit=4`).then((response) => {
+      if (!response) return;
+      var products = this.state.products;
+      if (products === null) products = [];      
+      var len = response.data.length;
+      for (let i = 0; i < len; i++) {
+        products.push(response.data[i]);
+      }
+      len = products.length;
+      var pages = [];
+      for (let i = 0; i < len; i = i + 20) {
+        pages.push(i);
+      }
+      this.setState({products: products, pages: pages, lastPage: this.state.lastPage + 1 });
+    });
+    } else if (offsetBottom <= 0 && this._prevOffsetBottom >= 0 && this.state.lastPage > 1) {
+      // var products = this.state.products;
+      // if (products === null) return;      
+      // var len = products.length;
+      // for (let i = 0; i < len; i++) {
+      //   products = products.slice(0, len - 5);
+      // }
+      // len = products.length;
+      // var pages = [];
+      // for (let i = 0; i < len; i = i + 20) {
+      //   pages.push(i);
+      // }
+      // this.setState({products: products, pages: pages, lastPage: this.state.lastPage - 1 });
+    }
+    this._prevOffsetBottom = offsetBottom;
+    // if (offsetTop < 0) {
+    //   //remove bottom row
+    // }
+  }
   componentDidMount() {
-    Axios.get("http://localhost:3001/products").then((response) => {
+    window.onscroll = this._handleScroll;
+    Axios.get("http://localhost:3001/products/?_page=1&_limit=4").then((response) => {
       if (!response) return;
       const len = response.data.length;
       var pages = [];
@@ -37,20 +88,22 @@ class Products extends Component {
         pages.push(i);
       }
       this.setState({products: response.data, pages: pages});
+      //this._handleScroll(true);
     });
   }
   render() {
-    console.log(this.state.pages);
     return (
       <div>{ this.state.products != null &&
         this.state.pages.map((page, i) => 
-          <div className="Container">{this.state.products.slice(page, page + 20).map((item, index) =>
-            <div key = {index} className="Grid-item">
-            <div className="Item-header">
-              <div className="Product-price">${item.price}</div>
+          <div key={i} className="products-container">{this.state.products.slice(page, page + 20).map((item, index) =>
+            <div onMouseOver={(e)=>console.log(e.currentTarget.getBoundingClientRect().y + 
+              e.currentTarget.getBoundingClientRect().height - document.documentElement.clientHeight)} 
+            key = {index} className="grid-item">
+            <div className="item-header">
+              <div className="product-price">${item.price}</div>
             </div>
-            <div className="Product-face">{item.face}</div>
-            <div className="Item-footer">
+            <div className="product-face">{item.face}</div>
+            <div className="item-footer">
               <div>Size:</div>
               <div>{item.size}</div>
               <div>Posted:</div>
@@ -59,8 +112,8 @@ class Products extends Component {
               <div>{item.id}</div>
             </div>
           </div>)}
-          <div className="Ad">
-            <img className="ad" src={"http://localhost:3001/ads/?r=" +  this._getNextAd()}/>
+          <div className="ad">
+            <img src={"/ads\/?r=" +  this._getNextAd()}/>
           </div>
         </div>
         )
