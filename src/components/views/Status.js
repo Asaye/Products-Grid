3bead84ce7fb6ba6f5e823b43433e4a28f88c18d
Asaye Dilbo <32922135+Asaye@users.noreply.github.com>
2019-12-17 11:02:56 +0300

    Add files via upload

---
+++ src/components/views/Status.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../css/status.css';
+
+class Status extends Component {  
+  render() {
+    return (
+      <div className="loading">loading
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    );
+  }
+}
+
+export default Status;
+
